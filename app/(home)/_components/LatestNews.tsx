7581bdaff62b4580e62895d8292bde6ebc9a1aec
Danilo Santos <dev.danilosantos@gmail.com>
2025-02-12 18:37:20 -0300

    feat: implement mobile slider for LatestNews component using Carousel

--- app/(home)/_components/LatestNews.tsx
+++ app/(home)/_components/LatestNews.tsx
@@ -1,8 +1,15 @@
 "use client";
 
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/app/_components/ui/carousel";
 import { useIsMobile } from "@/app/_hooks/use-mobile";
 import { Post } from "@prisma/client";
 import Image from "next/image";
+import Autoplay from "embla-carousel-autoplay";
+import { useRef } from "react";
 
 interface LatestNewsProps {
   posts: Post[];
@@ -10,38 +17,84 @@ interface LatestNewsProps {
 
 const LatestNews = ({ posts }: LatestNewsProps) => {
   const isMobile = useIsMobile();
+
+  const plugin = useRef(
+    Autoplay({
+      delay: 4000,
+      stopOnInteraction: false,
+      stopOnMouseEnter: true,
+    }),
+  );
+
   if (isMobile) {
-    return <div>Mobile</div>;
+    return (
+      <Carousel className="h-[300px] w-full" plugins={[plugin.current]}>
+        <CarouselContent className="">
+          {posts.map((post, index) => (
+            <CarouselItem
+              key={index}
+              className="group relative h-full cursor-pointer overflow-hidden"
+            >
+              <div className="relative h-[300px] w-full">
+                <Image
+                  src={post.imageUrl}
+                  alt={post.title}
+                  fill
+                  priority
+                  className="object-cover transition-all duration-300 group-hover:scale-110 md:object-center"
+                />
+              </div>
+              <div className="absolute bottom-2 z-10 w-full pb-2 pl-4">
+                <h2 className="w-[90%] truncate text-lg font-bold group-hover:text-secondary-foreground">
+                  {post.title}
+                </h2>
+                <p className="w-[90%] truncate">{post.content}</p>
+              </div>
+              <div className="absolute bottom-0 h-1/2 w-full bg-gradient-to-t from-black via-black/70" />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
+    );
   }
 
   return (
-    <div className="grid h-96 grid-cols-1 gap-2 lg:grid-cols-2">
-      <div className="relative">
+    <div className="grid h-[450px] grid-cols-1 gap-2 lg:grid-cols-2">
+      <div className="group relative cursor-pointer overflow-hidden lg:rounded-l-lg">
         <Image
           src={posts[0].imageUrl}
           alt={posts[0].title}
           fill
           sizes="100%"
-          className="object-cover md:object-top lg:rounded-l-lg"
+          priority
+          className="object-cover transition-all duration-300 group-hover:scale-110 md:object-top lg:rounded-l-lg"
         />
         <div className="absolute bottom-2 z-10 pb-2 pl-4">
-          <h2 className="text-lg font-bold">{posts[0].title}</h2>
+          <h2 className="text-lg font-bold group-hover:text-secondary-foreground">
+            {posts[0].title}
+          </h2>
           <p>{posts[0].content}</p>
         </div>
         <div className="absolute bottom-0 h-1/2 w-full bg-gradient-to-t from-black via-black/70" />
       </div>
       <div className="block h-full gap-2 space-y-2 lg:grid lg:space-y-0">
         {posts.slice(1).map((post) => (
-          <div key={post.id} className="relative h-full">
+          <div
+            key={post.id}
+            className="group relative h-full cursor-pointer overflow-hidden"
+          >
             <Image
               src={post.imageUrl}
               alt={post.title}
               fill
               sizes="100%"
-              className="object-cover md:object-center"
+              priority
+              className="object-cover transition-all duration-300 group-hover:scale-110 md:object-center"
             />
             <div className="absolute bottom-2 z-10 pb-2 pl-4">
-              <h2 className="text-lg font-bold">{post.title}</h2>
+              <h2 className="text-lg font-bold group-hover:text-secondary-foreground">
+                {post.title}
+              </h2>
               <p>{post.content}</p>
             </div>
             <div className="absolute bottom-0 h-1/2 w-full bg-gradient-to-t from-black via-black/70" />
