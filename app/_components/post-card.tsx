88afa0970bcfec3215ffb19d2a7d71138d5f92e5
Danilo Santos <dev.danilosantos@gmail.com>
2025-02-22 20:16:27 -0300

    feat: add comment count fetching in PostCard

--- app/_components/post-card.tsx
+++ app/_components/post-card.tsx
@@ -4,20 +4,24 @@ import Image from "next/image";
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
 import Link from "next/link";
+import getPostComments from "../_data-access/post/get-post-comments";
 
 interface PostCardProps {
   post: Post;
 }
 
-const PostCard = ({ post }: PostCardProps) => {
+const PostCard = async ({ post }: PostCardProps) => {
+  const comments = (await getPostComments(post.id)).length;
+
   return (
-    <Link href={`/posts/${post.id}`}>
-      <div className="group flex w-full cursor-pointer flex-col gap-2 md:flex-row">
+    <Link href={`/posts/${post.id}`} className="w-full">
+      <div className="group flex cursor-pointer flex-col gap-2 md:flex-row">
         <div className="relative flex h-36 w-full overflow-hidden rounded-lg md:max-h-[135px] md:min-h-[135px] md:min-w-[200px] md:max-w-[200px]">
           <Image
             src={post.imageUrl}
             alt={post.title}
             fill
+            sizes="100%"
             className="h-full w-full rounded-lg object-cover transition-all duration-300 group-hover:scale-110"
           />
         </div>
@@ -25,7 +29,7 @@ const PostCard = ({ post }: PostCardProps) => {
           <h1 className="w-11/12 truncate text-lg font-bold group-hover:text-secondary-foreground">
             {post.title}
           </h1>
-          <div className="flex gap-2">
+          <div className="flex w-full gap-2">
             <div className="flex items-center gap-2">
               <CalendarDaysIcon size={16} />
               <span className="text-xs">
@@ -34,10 +38,11 @@ const PostCard = ({ post }: PostCardProps) => {
             </div>
             <div className="flex items-center gap-1">
               <MessageCircleMore size={16} />
-              <span className="text-xs">0 comentários</span>
+              <span className="text-xs">{comments} comentário(s)</span>
             </div>
           </div>
-          <p className="w-11/12 truncate pt-4 text-muted-foreground md:w-[65%]">
+
+          <p className="line-clamp-2 w-full pt-4 text-muted-foreground lg:w-11/12">
             {post.content}
           </p>
         </div>
