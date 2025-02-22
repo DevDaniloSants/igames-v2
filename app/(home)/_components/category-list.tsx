5abb4b12e42884a4a71aefe38e1e5746dd0473f0
Danilo Santos <dev.danilosantos@gmail.com>
2025-02-22 20:19:29 -0300

    feat: add navigation to CategoryItem and  autoplay plugin to  CategoryList

--- app/(home)/_components/category-list.tsx
+++ app/(home)/_components/category-list.tsx
@@ -9,6 +9,8 @@ import {
 import CategoryItem from "./category-item";
 import { Category } from "@prisma/client";
 import { useIsMobile } from "@/app/_hooks/use-mobile";
+import { useRef } from "react";
+import Autoplay from "embla-carousel-autoplay";
 
 interface CategoryListProps {
   categories: Category[];
@@ -17,8 +19,16 @@ interface CategoryListProps {
 const CategoryList = ({ categories }: CategoryListProps) => {
   const isMobile = useIsMobile();
 
+  const plugin = useRef(
+    Autoplay({
+      delay: 2000,
+      stopOnInteraction: false,
+      stopOnMouseEnter: true,
+    }),
+  );
+
   return isMobile ? (
-    <Carousel className="max-w-1200px w-full">
+    <Carousel className="max-w-1200px w-full" plugins={[plugin.current]}>
       <CarouselContent>
         {categories.map((category) => (
           <CarouselItem
