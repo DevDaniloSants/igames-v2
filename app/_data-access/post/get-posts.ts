4cfa4d0d50ed6e97bce25e716b649eae1d3e702c
Danilo Santos <dev.danilosantos@gmail.com>
2025-03-06 20:55:03 -0300

    feat: add get-posts method to data-access layer

---
+++ app/_data-access/post/get-posts.ts
@@ -0,0 +1,28 @@
+"use server";
+
+import { db } from "@/app/_lib/prisma";
+import { Prisma } from "@prisma/client";
+
+export type GetPosts = Prisma.PostGetPayload<{
+  include: {
+    category: true;
+    author: true;
+  };
+}>;
+
+const getPosts = async (): Promise<GetPosts[]> => {
+  const post = await db.post.findMany({
+    include: {
+      category: true,
+      author: true,
+    },
+  });
+
+  if (!post) {
+    throw new Error("Post not found");
+  }
+
+  return post;
+};
+
+export default getPosts;
