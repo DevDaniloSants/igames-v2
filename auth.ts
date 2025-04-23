c58d8e16a2bb1982c245cc41528d9cca21b89c9b
Danilo Santos <dev.danilosantos@gmail.com>
2025-04-23 20:48:01 -0300

    feat: remove direct @auth/core dependency to avoid type conflicts with @auth/prisma-adapter

--- auth.ts
+++ auth.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 
-import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "./app/_lib/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 
 import authConfig from "./auth.config";
 
